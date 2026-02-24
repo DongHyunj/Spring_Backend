2fe80d3419ac71c250ffc95bf88319b237a36396
jdh <wjdehd555@gmail.com>
2026-02-24 15:41:43 +0900

    [Feat] 토큰 방식 로그인 기능 추가

--- src/main/java/org/example/backend/config/SecurityConfig.java
+++ src/main/java/org/example/backend/config/SecurityConfig.java
@@ -1,6 +1,7 @@
 package org.example.backend.config;
 
 import lombok.RequiredArgsConstructor;
+import org.example.backend.user.model.User;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
@@ -8,6 +9,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
@@ -33,7 +35,7 @@ public class SecurityConfig {
     }
 
     @Bean
-    public SecurityFilterChain configure(HttpSecurity http) throws Exception {
+    public SecurityFilterChain configure(HttpSecurity http, JwtFilter jwtFilter) throws Exception {
 
         http.cors(cors -> cors.configurationSource(corsConfigurationSource()));
 
@@ -46,6 +48,8 @@ public class SecurityConfig {
         http.csrf(AbstractHttpConfigurer::disable);
         http.httpBasic(AbstractHttpConfigurer::disable);
         http.formLogin(AbstractHttpConfigurer::disable);
+
+        http.addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
     }
